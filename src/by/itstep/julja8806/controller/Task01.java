8bec33d7e4eafa0d8f0212c1b4d59c88533286d8
yalo24 <olgarizkovic@gmail.com>
2022-09-22 11:21:57 +0300

    dragon view

--- src/by/itstep/julja8806/controller/Task01.java
+++ src/by/itstep/julja8806/controller/Task01.java
@@ -1,10 +1,25 @@
 package by.itstep.julja8806.controller;
 
+import by.itstep.julja8806.model.data.Dragon;
+import by.itstep.julja8806.model.logic.DragonLogic;
 import by.itstep.julja8806.view.Printer;
 
 public class Task01 {
     public static void main(String[] args) {
-        Printer.print("testing task01....");
+        Dragon garinich = new Dragon();
+        garinich.age = 120;
+        garinich.name = "Garinich";
 
+        Dragon pushok = new Dragon();
+        pushok.age = 350;
+        pushok.name = "Pushok";
+
+        int head = DragonLogic.calculateDragonHeads(garinich);
+        String msg = String.format("Garinich has %d heads\n", head);
+        Printer.print(msg);
+
+        head = DragonLogic.calculateDragonHeads(pushok);
+        msg = String.format("Pushok has %d heads\n", head);
+        Printer.print(msg);
     }
 }
